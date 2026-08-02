792e45260255fb0b40a64840484d3eaff4cd157b
A Akash Rao <aakashrao794@gmail.com>
2026-08-02 23:08:45 +0530

    Create students.js

---
+++ data/students.js
@@ -0,0 +1,16 @@
+let students = [
+    {
+        id: 1,
+        name: "Akash Rao",
+        age: 20,
+        course: "BCA"
+    },
+    {
+        id: 2,
+        name: "Rahul",
+        age: 21,
+        course: "B.Tech"
+    }
+];
+
+module.exports = students;
